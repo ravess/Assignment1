63ffb32fce160befa6d5112cbae340265163ccf3
Roy <weichai.royston.yeo@stengg.com>
2023-07-10 14:57:11 +0800

    added utils for error handler and jwtToken

---
+++ utils/jwtToken.js
@@ -0,0 +1,23 @@
+// Create and send token and save in cookie
+const sendToken = (user, statusCode, res) => {
+  // Create JWT Token
+  const token = user.getJwtToken();
+  // Options for cookie
+  const options = {
+    expires: new Date(
+      Date.now() + process.env.COOKIE_EXPIRES_TIME * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+  };
+
+  // Can enable if we using https
+  // if (process.env.NODE_ENV === 'production') {
+  //   options.secure = true;
+  // }
+  res
+    .status(statusCode)
+    .cookie("token", token, options)
+    .json({ success: true, token });
+};
+
+module.exports = sendToken;
