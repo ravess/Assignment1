754459da38d2d53c7d80b70bdcba7c7aef739372
Roy <weichai.royston.yeo@stengg.com>
2023-07-17 11:53:33 +0800

    amend some stuff

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -1,4 +1,4 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 // Create and send token and save in cookie
 const sendToken = (user, statusCode, res) => {
   // Create JWT Token
@@ -11,12 +11,13 @@ const sendToken = (user, statusCode, res) => {
       Date.now() + process.env.COOKIE_EXPIRES_TIME * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
+    secure: true,
   };
   console.log(token, `from sendToken`);
   res
     .status(statusCode)
-    .cookie('token', token, options)
-    .json({ success: true, token });
+    .cookie("token", token, options)
+    .json({ success: true });
 };
 
 module.exports = sendToken;
